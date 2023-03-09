06289e8695c360bc0ea119c734e53537606b3c58
Al Rodriguez <ProgrammerAl@Outlook.com>
2023-03-09 16:42:44 -0500

    Added container name to attribute

--- ClientLib/src/ClientLib/IdConfiguredEntityAttribute.cs
+++ ClientLib/src/ClientLib/IdConfiguredEntityAttribute.cs
@@ -3,4 +3,10 @@
 [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
 public class IdConfiguredEntityAttribute : Attribute
 {
+    public IdConfiguredEntityAttribute(string containerName)
+    { 
+        ContainerName = containerName;
+    }
+
+    public string ContainerName { get; set; }
 }
